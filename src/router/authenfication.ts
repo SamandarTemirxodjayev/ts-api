db6ed40e7e0935ef865756683e7b71fc9cbb1674
SamandarTemirxodjayev <iwtuz166@gmail.com>
2023-04-07 14:21:22 +0500

    change get to post

--- src/router/authenfication.ts
+++ src/router/authenfication.ts
@@ -3,5 +3,5 @@ import express from 'express'
 import { register } from '../controllers/authentication'
 
 export default (router: express.Router) => {
-  router.get('/auth/register', register)
+  router.post('/auth/register', register)
 }
